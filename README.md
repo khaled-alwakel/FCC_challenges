976fa1c7d29bf025f569acdd53c31f01005ccab1
khaled-alwakel <khaledmofreh33@gmail.com>
2023-08-21 14:14:24 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FCC_challenges
+
+## cashRegister:
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
--- cashRegister.js
+++ cashRegister.js
@@ -1,4 +1,4 @@
-// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
+
 
 let CurrencyUnit = {
   PENNY: 0.01,
